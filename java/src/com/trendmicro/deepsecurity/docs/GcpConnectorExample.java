cca420d781d9bf7ea49aa616a405c3c61d5e672c
ChristinaSmith123 <35069535+ChristinaSmith123@users.noreply.github.com>
2019-09-10 14:02:24 -0400

    Update GcpConnectorExample.java
    Clean up tabs.
    Clean up comments. Used tabs to space things out a bit.

--- java/src/com/trendmicro/deepsecurity/docs/GcpConnectorExample.java
+++ java/src/com/trendmicro/deepsecurity/docs/GcpConnectorExample.java
@@ -9,31 +9,31 @@ import com.trendmicro.deepsecurity.model.Action;
 import com.trendmicro.deepsecurity.model.GCPConnector;
 
 /**
- * the examples of gcp connector 
+ * Examples of the GCP connector 
  */
 public class GcpConnectorExample {
 
 	/**
-	 * create a gcp connector
+	 * Create a gcp connector.
 	 * 
-	 * @param name           the name of the gcp connector
-	 * @param serviceAccount the base64 string of the gcp service account
-	 * @param apiVersion     The version of the API to use.
-	 * @return the GCPConnector object
-	 * @throws ApiException if a problem occurs when creating a gcp connector on
-	 *                      Deep Security Manager.
+	 * @param name          	The name of the GCP connector.
+	 * @param serviceAccount	The base64 string of the GCP service account.
+	 * @param apiVersion    	The version of the API to use.
+	 * @return 			Return the GCPConnector object.
+	 * @throws ApiException 	Throw an exception if a problem occurs when creating a GCP connector on
+	 *                       	Deep Security Manager.
 	 */
 	public static GCPConnector createGcpConnector(String name, String serviceAccount, String apiVersion)
 			throws ApiException {
 
 		GcpConnectorsApi apiInstance = new GcpConnectorsApi();
 
-		// Create a GCPConnector
+		// Create a GCP connector
 		GCPConnector gcpConnector = new GCPConnector();
 		gcpConnector.setName(name);
 		gcpConnector.setServiceAccount(serviceAccount);
 
-		// Add a GcpConnector to Deep Security Manager
+		// Add a GCP connector to Deep Security Manager
 		GCPConnector result = null;
 		result = apiInstance.createGCPConnector(gcpConnector, apiVersion);
 
@@ -41,12 +41,12 @@ public class GcpConnectorExample {
 	}
 
 	/**
-	 * submit a sync action to the gcp connector
-	 * @param gcpConnectorID the target gcpConnectorID 
-	 * @param apiVersion The version of the API to use.
-	 * @return the action object which contains the action status.
-	 * @throws ApiException if a problem occurs when creating a gcp connector on
-	 *                      Deep Security Manager.
+	 * Submit a sync action to the GCP connector.
+	 * @param gcpConnectorID 	The target gcpConnectorID.
+	 * @param apiVersion 		The version of the API to use.
+	 * @return 			Return the action object which contains the action status.
+	 * @throws ApiException		Throw an exception if a problem occurs when creating a GCP connector on
+	 *                      	Deep Security Manager.
 	 */
 	public static Action submitGCPConnectorSyncAction(Integer gcpConnectorID, String apiVersion) throws ApiException {
 		
