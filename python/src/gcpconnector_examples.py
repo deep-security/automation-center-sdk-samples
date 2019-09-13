7264a579dcc8d85e5404ca122307226632616aa0
Christina Smith <christina_smith@trendmicro.com>
2019-09-13 10:29:18 -0400

    bug/DS-41052_GCP_Add_Comments Fix GCP Comments

--- python/src/gcpconnector_examples.py
+++ python/src/gcpconnector_examples.py
@@ -13,22 +13,49 @@
 # limitations under the License.
 #
 
+
+
 def create_gcp_connector(api, configuration, api_version, api_exception, name, service_account):
+    """ Creates a GCP connector.
+    :param api              The Deep Security API exports.
+    :param configuration    The configuration object to pass to the API client.
+    :param api_version      The API version to use.
+    :param api_exception    The Deep Security API exception module.
+    :param name             The name of the GCP connector.
+    :param service_account  The GCP service account used by the GCP connector.
+    :return                 The created GCP connector object which contains the created GCP connector ID information.
+    """
+    
+    # Create a GCP connector object
     api_instance = api.GCPConnectorsApi(api.ApiClient(configuration))
     gcp_connector = api.GCPConnector()
+    
+    # Set the GCP connector properties
     gcp_connector.name = name
     gcp_connector.service_account = service_account
     try:
+        # Call create_gcp_connector API to create the GCP connector
         api_response = api_instance.create_gcp_connector(gcp_connector, api_version)
         return api_response
     except api_exception as e:
         print("An exception occurred when calling GCPConnectorsApi.create_google_connector: %s\n" % e)
 
 def submit_gcp_connector_sync_action(api, configuration, api_version, api_exception, gcp_connector_id):
+    """ Submits a synchronize action of a GCP connector.
+    :param api              The Deep Security API exports.
+    :param configuration    The configuration object to pass to the API client.
+    :param api_version      The API version to use.
+    :param api_exception    The Deep Security API exception module.
+    :param gcp_connector_id The GCP connector ID of the target GCP connector.
+    :return                 The created Action object which contains the ID and status of the action.
+    """
+    
+    # Create the GCPConnectorActionsApi instance and an Action object to synchronize the GCP connector
     api_instance = api.GCPConnectorActionsApi(api.ApiClient(configuration))
     gcp_connector_action = api.Action()
     gcp_connector_action.type = "synchronize"
     try:
+        # Call the create_gcp_connector_action API to create a synchronize action for the target GCP connector
         api_response = api_instance.create_gcp_connector_action(gcp_connector_id, gcp_connector_action, api_version)
         return api_response
     except api_exception as e:
